5c76d31ff759733691c83bb17959facbdb023c84
hoangphong <hoangphong@macbook-cua-phong.local>
2023-04-26 14:53:50 +0700

    update

---
+++ src/main/java/com/web/monolithic/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.web.monolithic.repository;
+
+import com.web.monolithic.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
